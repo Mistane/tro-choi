4cc6fbe37a8db467aa1235fb73cf1d16eebe0b35
Mistane <mistanevn@gmail.com>
2024-11-24 21:55:52 +0700

    phat sung thu 2

--- main.js
+++ main.js
@@ -1,47 +1,130 @@
-
-const words = ["Hifumi daisuki", "dark dark bruh bruh", "shinonome ena"];
+const words = [
+  "chua tri",
+  "mia mai",
+  "do khoc do cuoi",
+  "gay cuoi",
+  "phe phan",
+  "dao duc",
+];
 const rows = document.querySelector(".container__row");
 const button = document.querySelector(".btn");
 
-display = function(){
-    words.map((word) => {
-        let rowAnswer = document.createElement("div");
-        rowAnswer.classList.add("row__ans");
+display = function () {
+  words.map((word, index) => {
+    let rowAnswer = document.createElement("div");
+    rowAnswer.classList.add("row__ans");
+    rowAnswer.classList.add(`row${index + 1}`);
+    rowAnswer.setAttribute("data-set", `${index}`);
 
-        let rowBG = document.createElement("div");
-        rowBG.classList.add("row__ans--background");
-        for(let i = 0; i < word.length; i++){
-            //tao row box
-            let rowBox = document.createElement("span");
-            rowBox.classList.add("row__box");
+    let rowBG = document.createElement("div");
+    rowBG.classList.add("row__ans--background");
+    for (let i = 0; i < word.length; i++) {
+      //tao row box
+      let rowBox = document.createElement("span");
+      rowBox.classList.add("row__box");
 
-            //tao background box
-            let backgroundBox = document.createElement("span");
-            backgroundBox.classList.add("background__box");
+      //tao background box
+      let backgroundBox = document.createElement("span");
+      backgroundBox.classList.add("background__box");
 
-            if(word[i] == ' ') continue;
-            rowBox.innerHTML = `${word[i]}`;
-            console.log(rowBox);
-            rowAnswer.append(rowBox);
-            rowBG.append(backgroundBox);
-            
-        }
-        rowAnswer.append(rowBG);
-        rows.append(rowAnswer);
-    })
-}
+      if (word[i] == " ") continue;
+      rowBox.innerHTML = `${word[i]}`;
+      // console.log(rowBox);
+      rowAnswer.append(rowBox);
+      rowBG.append(backgroundBox);
+    }
+    rowAnswer.append(rowBG);
+    rows.append(rowAnswer);
+  });
+};
 
 display();
 
-const answers = document.querySelectorAll(".row__ans");
+// const answers = document.querySelectorAll(".row__ans");
 
-answers.forEach((answer) => {
-    answer.addEventListener("click", ()=>{
-        const background = answer.querySelector(".row__ans--background");
-        const backgroundBox = answer.querySelectorAll(".background__box");
-        background.classList.toggle("active");
-        backgroundBox.forEach((box)=>{
-            box.classList.toggle(".active");
-        })
-    })  
-})
+// answers.forEach((answer) => {
+//     answer.addEventListener("click", ()=>{
+//         const background = answer.querySelector(".row__ans--background");
+//         const backgroundBox = answer.querySelectorAll(".background__box");
+//         background.classList.toggle("active");
+//         backgroundBox.forEach((box)=>{
+//             box.classList.toggle(".active");
+//         })
+//     })
+// })
+
+//Fisher-Yates algorithm
+function swap(string) {
+  let arr = string.split("");
+
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr.join(" / ");
+}
+
+function change(string) {
+  let arr = "";
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === " ") continue;
+    arr += string[i];
+  }
+  return arr;
+}
+
+const panel = document.querySelector(".button-container");
+const row__answer = document.querySelectorAll(".row__ans");
+let panelSelected = false;
+
+row__answer.forEach((row) => {
+  row.addEventListener("click", (e) => {
+    if (panelSelected) return;
+
+    panel.style.display = "flex";
+    panelSelected = true;
+    const value = e.currentTarget.dataset.set;
+    console.log(value);
+    panel.innerHTML = `<div class="answer__panel">
+            <p class="answer__msg"></p>
+            <div class="answer__exit"><i class="fa-solid fa-x"></i></div>
+            <p class="answer__hint">${swap(change(words[value]))}</p>
+            <div class="answer__nav">
+                <button type="submit" class="answer__submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                <input type="text" class="answer__input" id="answer__input" placeholder="Không cho 10đ -> cắt đồng" autocomplete="off">
+            </div>
+        </div>`;
+
+    const tmp = words[value];
+    const answer__msg = document.querySelector(".answer__msg");
+    const submit = document.querySelector(".answer__submit");
+    const inputField = document.querySelector(".answer__input");
+    const exit = document.querySelector(".answer__exit");
+    const check = row.querySelector(".row__ans--background");
+
+    submit.addEventListener("click", handleSubmit);
+
+    function handleSubmit() {
+      const answer = inputField.value;
+      console.log(answer);
+      console.log(answer === tmp);
+      if (answer == tmp) {
+
+        answer__msg.innerHTML = "Gioi";
+        check.classList.add("active");
+        panelSelected = false;
+      } else{
+        answer__msg.innerHTML = "Kem";
+        // row.classList.add("fail");
+      }
+        
+      inputField.value = "";
+    }
+
+    exit.addEventListener("click", ()=>{
+      panel.style.display = "none";
+      panelSelected = false;
+    })
+  });
+});
